66c66ae67d5c62a5533e5f92f587a0e1f471e9a1
Yuhan Chen <chenyh@umich.edu>
2022-02-22 21:46:08 -0500

    Add ProcessPoolExecutor support for er sparsifier

--- sparsifier/er_sparsify.py
+++ sparsifier/er_sparsify.py
@@ -1,5 +1,6 @@
 import os
 import os.path as osp
+import sys
 import pickle
 
 import numpy as np
@@ -13,6 +14,7 @@ from numpy import inf
 from torch_geometric.utils import get_laplacian, from_scipy_sparse_matrix
 from torch_sparse import SparseTensor
 import myLogger
+from concurrent.futures import ProcessPoolExecutor
 
 npz_file_path = None
 csv_file_path = None
@@ -36,6 +38,7 @@ def stage1(dataset):
     Input:
         dataset: PygDataset
     """
+    myLogger.info(message=f"Computing ER Stage 1")
     if not osp.exists(npz_file_path):
         sparse_transform = ToSparseTensor()
         sparse_t_data = sparse_transform(dataset.data)
@@ -48,14 +51,49 @@ def stage2():
     """
     Stage 2: Run the Julia script that loads the npz file and generates the V matrix
     """
-    if not os.exists(csv_file_path):
+    myLogger.info(message=f"Computing ER Stage 2")
+    if not osp.exists(csv_file_path):
         cwd = os.getcwd()
         current_file_dir = osp.dirname(osp.realpath(__file__))
         os.chdir(current_file_dir)
         os.system(f"julia compute_V.jl --filepath={npz_file_path}")
         os.chdir(cwd)
 
-def stage3(dataset):
+def compute_edge_data(epsilon: str, Pe, C, weights, start_nodes, end_nodes, N, dataset_name, config):
+    if not isinstance(epsilon, str):
+        myLogger.error(message=f"epsilon must be a str, quitting...")
+        sys.exit(1)
+    q = round(N * np.log(N) * 9 * C ** 2 / (float(epsilon) ** 2))
+    results = np.random.choice(np.arange(np.shape(Pe)[0]), int(q), p=list(Pe))
+    spin_counts = stats.itemfreq(results).astype(int)
+
+    per_spin_weights = weights / (q * Pe)
+    per_spin_weights[per_spin_weights == inf] = 0
+
+    counts = np.zeros(np.shape(weights)[0])
+    counts[spin_counts[:, 0]] = spin_counts[:, 1]
+    new_weights = counts * per_spin_weights
+
+    sparserW = sparse.csc_matrix(
+        (np.squeeze(new_weights), (start_nodes, end_nodes)), shape=(N, N)
+    )
+    sparserW = sparserW + sparserW.T
+    print(f"Prune rate for epsilon={epsilon}: ", 1 - np.count_nonzero(new_weights) / np.size(new_weights))
+    # convert into PyG's object
+    edge_index, edge_weight = from_scipy_sparse_matrix(sparserW)
+    if epsilon in config[dataset_name]["er_epsilon_to_drop_rate_map"]:
+        prune_file_path = osp.join(prune_file_dir, str(config[dataset_name]["er_epsilon_to_drop_rate_map"][epsilon]), "edge_data.pt")
+        os.makedirs(osp.dirname(prune_file_path), exist_ok=True)
+        torch.save({"edge_index": edge_index, "edge_weight": edge_weight}, prune_file_path)
+        myLogger.info(message=f"Saving edge_data.pt for future use")
+    else:
+        myLogger.info(message=f"Drop rate for epsilon={epsilon} not found in config, edge_data.pt not saved")
+        
+    return edge_index, edge_weight
+    
+    
+def stage3(dataset, dataset_name, epsilon: float | str | list[float] | list[str], config):
+    myLogger.info(message=f"Computing ER Stage 3")
     V_frame = pd.read_csv(csv_file_path, header=None)
     myLogger.info(message=f"Loaded V.csv")
     V = V_frame.to_numpy()
@@ -72,7 +110,7 @@ def stage3(dataset):
         R_eff = compute_reff(W_scipy, V)
         with open(pkl_file_path, "wb") as f:
             pickle.dump(R_eff, f)
-            print( f"Saved effective resistances in Reff.pkl for future use.")
+            myLogger.info(message=f"Saved effective resistances in Reff.pkl for future use.")
             f.close()
     else:
         with open(pkl_file_path, "rb") as f:
@@ -93,53 +131,29 @@ def stage3(dataset):
     # Rudelson and Vershynin, 2007, Thm. 3.1
     C = 4 * C0
 
-    # 0.094 -> 0.1
-    # 0.122 -> 0.2
-    # 0.148 -> 0.3
-    # 0.176 -> 0.4
-    # 0.209 -> 0.5
-    # 0.25 -> 0.6
-    # 0.306 -> 0.7
-    # 0.395 -> 0.8
-    # 0.594 -> 0.9
-    # 0.87 -> 0.95
-    epsilons = [0.094, 0.122, 0.148, 0.176, 0.209, 0.25, 0.306, 0.395, 0.594, 0.87]
-    for epsilon in epsilons:
-        print("\n------------------------------------------------------")
-        print(f"Epsilon: {epsilon}")
-        q = round(N * np.log(N) * 9 * C ** 2 / (epsilon ** 2))
-        results = np.random.choice(np.arange(np.shape(Pe)[0]), int(q), p=list(Pe))
-        spin_counts = stats.itemfreq(results).astype(int)
-
-        per_spin_weights = weights / (q * Pe)
-        per_spin_weights[per_spin_weights == inf] = 0
-
-        counts = np.zeros(np.shape(weights)[0])
-        counts[spin_counts[:, 0]] = spin_counts[:, 1]
-        new_weights = counts * per_spin_weights
-
-        sparserW = sparse.csc_matrix(
-            (np.squeeze(new_weights), (start_nodes, end_nodes)), shape=(N, N)
-        )
-        sparserW = sparserW + sparserW.T
-        print("Edges (before sparsification): ", np.count_nonzero(weights))
-        print("Edges (after sparsification): ", np.count_nonzero(new_weights))
-        print("Prune rate: ", 1 - np.count_nonzero(new_weights) / np.size(new_weights))
-        # convert into PyG's object
-        edge_index, edge_weight = from_scipy_sparse_matrix(sparserW)
-        torch.save(
-            {"edge_index": edge_index, "edge_weight": edge_weight},
-            osp.join(prune_file_dir, epsilon, "edge_data.pt")
-            )
-        
+    if isinstance(epsilon, float) or isinstance(epsilon, str):
+        edge_index, edge_weight = compute_edge_data(str(epsilon), Pe, C, weights, start_nodes, end_nodes, N, dataset_name, config)
+    elif isinstance(epsilon, list[float]) or isinstance(epsilon, list[str]):
+        with ProcessPoolExecutor(max_workers=64) as executor:
+            futures = {executor.submit(compute_edge_data, epsilon_, Pe, C, weights, start_nodes, end_nodes, N, dataset_name, config): epsilon_ for epsilon_ in epsilon}
+            for future in futures:
+                print(f"Epsilon: {futures[future]}")
+                future.result()
+        edge_index, edge_weight = None, None
+    else:
+        myLogger.error(message=f"epsilon must be one of the type: float, str, list[float], list[str]")
+        sys.exit(1)
     return  edge_index, edge_weight
 
-def er_sparsify(dataset, dataset_name, epsilon):
+
+def er_sparsify(dataset, dataset_name, epsilon: float | str | list[float] | list[str], config):
     """
     Input:
         dataset: PygDataset
         dataset_name: str, name of the dataset
-        epsilon: float, sparsification parameter
+        epsilon: float | str -> return edge_index, edge_weight
+                 list[float] | list[str] -> compute each epsilon, no return
+        config: config dict
     Output:
         data
     """
@@ -149,16 +163,29 @@ def er_sparsify(dataset, dataset_name, epsilon):
     pkl_file_path = osp.join(osp.dirname(osp.abspath(__file__)), f'../data/{dataset_name}/raw/Reff.pkl')
     prune_file_dir = osp.join(osp.dirname(osp.abspath(__file__)), f'../data/{dataset_name}/pruned/er')
     os.makedirs(prune_file_dir, exist_ok=True)
-   
-    if osp.exists(ops.join(prune_file_dir, epsilon, "edge_data.pt")):
-        edge_data = torch.load(ops.join(prune_file_dir, epsilon, "edge_data.pt"))
-        edge_index = edge_data["edge_index"]
-        edge_weight = edge_data["edge_weight"]
-    else:
+    
+    if isinstance(epsilon, float) or isinstance(epsilon, str): 
+        prune_file_path = osp.join(prune_file_dir, str(epsilon), "edge_data.pt")
+        if osp.exists(prune_file_path):
+            myLogger.info(message=f"edge_data.pt already exists. Loading it...")
+            edge_data = torch.load(prune_file_path)
+            edge_index = edge_data["edge_index"]
+            edge_weight = edge_data["edge_weight"]
+        else:
+            os.makedirs(osp.dirname(prune_file_path), exist_ok=True)
+            myLogger.info(message=f"edge_data.pt does not exist. Computing it...")
+            stage1(dataset)
+            stage2()
+            edge_index, edge_weight = stage3(dataset, dataset_name, epsilon, config)
+    elif isinstance(epsilon, list[float]) or isinstance(epsilon, list[str]):
+        myLogger.info(message=f"edge_data.pt does not exist. Computing it...")
         stage1(dataset)
         stage2()
-        stage3(dataset)
-    data = dataset.data
+        stage3(dataset, dataset_name, epsilon, config)
+    else:
+        myLogger.error(message=f"epsilon must be one of the type: float, str, list[float], list[str]")
+        sys.exit(1)
+
     data.edge_index = edge_index
     data.edge_weight = edge_weight
     return data
