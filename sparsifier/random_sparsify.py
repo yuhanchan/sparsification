4ee69beb972ceb8bd00ee5b8c86098f4c066182a
Yuhan Chen <chenyh@umich.edu>
2022-07-11 14:55:45 -0400

    style: format code

--- sparsifier/random_sparsify.py
+++ sparsifier/random_sparsify.py
@@ -4,50 +4,68 @@ import myLogger
 import os.path as osp
 import os
 import sys
-from concurrent.futures import ProcessPoolExecutor 
+from concurrent.futures import ProcessPoolExecutor
 
 
 # set random seed for deterministic results
 torch.manual_seed(1)
 np.random.seed(1)
 
+
 def random_sparsify(dataset, dataset_name, drop_rate):
     """
     Input:
         dataset: PygDataset
         dataset_name: str, name of dataset
-        drop_rate: float, between 0 and 1 
+        drop_rate: float, between 0 and 1
     Output:
         dataset: PygDataset, with edges randomly dropped
     """
-    myLogger.info(f'Getting random prune file with prune rate {drop_rate} for {dataset_name}')
-    prune_file_path = osp.join(osp.dirname(osp.abspath(__file__)), f'../data/{dataset_name}/pruned/random/{drop_rate}/edge_selection.npy')
-    duw_el_path = osp.join(osp.dirname(osp.abspath(__file__)), f'../data/{dataset_name}/pruned/random/{drop_rate}/duw.el')
-    uduw_el_path = osp.join(osp.dirname(osp.abspath(__file__)), f'../data/{dataset_name}/pruned/random/{drop_rate}/uduw.el')
+    myLogger.info(
+        f"Getting random prune file with prune rate {drop_rate} for {dataset_name}"
+    )
+    prune_file_path = osp.join(
+        osp.dirname(osp.abspath(__file__)),
+        f"../data/{dataset_name}/pruned/random/{drop_rate}/edge_selection.npy",
+    )
+    duw_el_path = osp.join(
+        osp.dirname(osp.abspath(__file__)),
+        f"../data/{dataset_name}/pruned/random/{drop_rate}/duw.el",
+    )
+    uduw_el_path = osp.join(
+        osp.dirname(osp.abspath(__file__)),
+        f"../data/{dataset_name}/pruned/random/{drop_rate}/uduw.el",
+    )
     if osp.exists(prune_file_path):
-        myLogger.info(message=f'Prune file already exists, loading edge selection')
+        myLogger.info(message=f"Prune file already exists, loading edge selection")
         edge_selection = torch.load(prune_file_path)
     else:
-        myLogger.info(message=f'Prune file not exist, generating random prune file with drop rate {drop_rate}')
-        if dataset_name in ['Reddit', 'Reddit2', 'ogbn_products']:
+        myLogger.info(
+            message=f"Prune file not exist, generating random prune file with drop rate {drop_rate}"
+        )
+        if dataset_name in ["Reddit", "Reddit2", "ogbn_products"]:
             size = dataset.data.edge_index.shape[1]
         else:
-            myLogger.info(message=f'{dataset_name} is not supported. Exiting...')
+            myLogger.info(message=f"{dataset_name} is not supported. Exiting...")
             sys.exit(1)
-        edge_selection = torch.tensor(np.random.binomial(1, 1.0 - drop_rate, size=size)).type(torch.bool)
+        edge_selection = torch.tensor(
+            np.random.binomial(1, 1.0 - drop_rate, size=size)
+        ).type(torch.bool)
         os.makedirs(osp.dirname(prune_file_path), exist_ok=True)
         torch.save(edge_selection, prune_file_path)
-        myLogger.info(message=f'Prune file saved for future use')
-        
+        myLogger.info(message=f"Prune file saved for future use")
+
     data = dataset.data
     data.edge_index = data.edge_index[:, edge_selection]
     if not osp.exists(duw_el_path):
-        np.savetxt(duw_el_path, data.edge_index.numpy().transpose().astype(int), fmt='%i')
+        np.savetxt(
+            duw_el_path, data.edge_index.numpy().transpose().astype(int), fmt="%i"
+        )
     if not osp.exists(uduw_el_path):
         to_save = data.edge_index.numpy().transpose().astype(int)
-        with open(uduw_el_path, 'w') as f:
+        with open(uduw_el_path, "w") as f:
             for line in to_save:
-                f.write(f'{line[0]} {line[1]}\n') if line[0] < line[1] else None
+                f.write(f"{line[0]} {line[1]}\n") if line[0] < line[1] else None
     dataset.data = data
     return dataset
 
@@ -55,7 +73,7 @@ def random_sparsify(dataset, dataset_name, drop_rate):
 def cpp_random_sparsify(dataset_name, drop_rate, config=None, directed=True):
     """
     Input:
-        dataset_name: str, name of dataset 
+        dataset_name: str, name of dataset
         drop_rate: float, between 0 and 1
         config: loaded from config.json
         directed: bool, if True, take edge list as is
@@ -64,39 +82,57 @@ def cpp_random_sparsify(dataset_name, drop_rate, config=None, directed=True):
     cwd = os.getcwd()
     current_file_dir = os.path.dirname(os.path.realpath(__file__))
 
-    myLogger.info(f'Generatin random prune file with drop_rate {drop_rate} for {dataset_name}')
-    if config == None or drop_rate not in config[dataset_name]['drop_rate']:
-        myLogger.error(f'{drop_rate} not in config[{dataset_name}][drop_rate]')
+    myLogger.info(
+        f"Generatin random prune file with drop_rate {drop_rate} for {dataset_name}"
+    )
+    if config == None or drop_rate not in config[dataset_name]["drop_rate"]:
+        myLogger.error(f"{drop_rate} not in config[{dataset_name}][drop_rate]")
         sys.exit(1)
 
-    duw_el_path = os.path.join(current_file_dir, 
-                            f'../data/{dataset_name}/pruned/random/', 
-                            f'{drop_rate}/duw.el')
-    uduw_el_path = os.path.join(current_file_dir, 
-                            f'../data/{dataset_name}/pruned/random/', 
-                            f'{drop_rate}/uduw.el')
+    duw_el_path = os.path.join(
+        current_file_dir,
+        f"../data/{dataset_name}/pruned/random/",
+        f"{drop_rate}/duw.el",
+    )
+    uduw_el_path = os.path.join(
+        current_file_dir,
+        f"../data/{dataset_name}/pruned/random/",
+        f"{drop_rate}/uduw.el",
+    )
     os.makedirs(os.path.dirname(duw_el_path), exist_ok=True)
-    
+
     os.chdir(current_file_dir)
     if directed:
-        input_el_path = os.path.join(current_file_dir, f'../data/{dataset_name}/raw/duw.el')  
-        os.system(f'./bin/prune -f {input_el_path} -q random -p {drop_rate} -o {duw_el_path}')
+        input_el_path = os.path.join(
+            current_file_dir, f"../data/{dataset_name}/raw/duw.el"
+        )
+        os.system(
+            f"./bin/prune -f {input_el_path} -q random -p {drop_rate} -o {duw_el_path}"
+        )
     else:
-        input_el_path = os.path.join(current_file_dir, f'../data/{dataset_name}/raw/uduw.el')
-        os.system(f'./bin/prune -f {input_el_path} -q random -p {drop_rate} -o {uduw_el_path}')
+        input_el_path = os.path.join(
+            current_file_dir, f"../data/{dataset_name}/raw/uduw.el"
+        )
+        os.system(
+            f"./bin/prune -f {input_el_path} -q random -p {drop_rate} -o {uduw_el_path}"
+        )
     os.chdir(cwd)
+
 
 def cpp_random_sparsify_all(dataset_name, config, directed=True):
     with ProcessPoolExecutor(max_workers=2) as executor:
         futures = {}
-        for drop_rate in config[dataset_name]['drop_rate']:
-            futures[executor.submit(el_random_sparsify, dataset_name, drop_rate, config, directed)] = drop_rate
+        for drop_rate in config[dataset_name]["drop_rate"]:
+            futures[
+                executor.submit(
+                    el_random_sparsify, dataset_name, drop_rate, config, directed
+                )
+            ] = drop_rate
         for future in futures:
-            print(f'start {futures[future]}')
+            print(f"start {futures[future]}")
             try:
                 future.result()
             except Exception as e:
                 print(e)
-                print(f'failed {futures[future]}')
+                print(f"failed {futures[future]}")
                 sys.exit(1)
-                
