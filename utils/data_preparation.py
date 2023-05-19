9187b5f820d1d0b40d467683572bf5b806c00785
Yuhan Chen <chenyh@umich.edu>
2023-05-19 19:38:26 -0400

    feat: unpack after download

--- utils/data_preparation.py
+++ utils/data_preparation.py
@@ -42,30 +42,43 @@ else:
 for dataset_name in dataset_names:
     if dataset_name == "ego-Facebook":
         os.system(f"curl https://snap.stanford.edu/data/facebook_combined.txt.gz --create-dirs -o {PROJECT_HOME}/data/ego-Facebook/raw/facebook_combined.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/ego-Facebook/raw/facebook_combined.txt.gz")
     if dataset_name == "ego-Twitter":
         os.system(f"curl https://snap.stanford.edu/data/twitter_combined.txt.gz --create-dirs -o {PROJECT_HOME}/data/ego-Twitter/raw/twitter_combined.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/ego-Twitter/raw/twitter_combined.txt.gz")
     if dataset_name == "human_gene2":
         os.system(f"curl https://suitesparse-collection-website.herokuapp.com/MM/Belcastro/human_gene2.tar.gz --create-dirs -o {PROJECT_HOME}/data/human_gene2/raw/human_gene2.txt.gz")
+        os.system(f"tar -xvf {PROJECT_HOME}/data/human_gene2/raw/human_gene2.tar.gz -C {PROJECT_HOME}/data/human_gene2/raw/")
     if dataset_name == "com-DBLP":
         os.system(f"curl https://snap.stanford.edu/data/bigdata/communities/com-dblp.ungraph.txt.gz --create-dirs -o {PROJECT_HOME}/data/com-DBLP/raw/com-dblp.ungraph.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/com-DBLP/raw/com-dblp.ungraph.txt.gz")
     if dataset_name == "com-Amazon":
         os.system(f"curl https://snap.stanford.edu/data/bigdata/communities/com-amazon.ungraph.txt.gz --create-dirs -o {PROJECT_HOME}/data/com-Amazon/raw/com-amazon.ungraph.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/com-Amazon/raw/com-amazon.ungraph.txt.gz")
     if dataset_name == "email-Enron":
         os.system(f"curl https://snap.stanford.edu/data/email-Enron.txt.gz --create-dirs -o {PROJECT_HOME}/data/email-Enron/raw/email-Enron.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/email-Enron/raw/email-Enron.txt.gz")
     if dataset_name == "wiki-Talk":
         os.system(f"curl https://snap.stanford.edu/data/wiki-Talk.txt.gz --create-dirs -o {PROJECT_HOME}/data/wiki-Talk/raw/wiki-Talk.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/wiki-Talk/raw/wiki-Talk.txt.gz")
     if dataset_name == "ca-AstroPh":
         os.system(f"curl https://snap.stanford.edu/data/ca-AstroPh.txt.gz --create-dirs -o {PROJECT_HOME}/data/ca-AstroPh/raw/ca-AstroPh.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/ca-AstroPh/raw/ca-AstroPh.txt.gz")
     if dataset_name == "ca-HepPh":
         os.system(f"curl https://snap.stanford.edu/data/ca-HepPh.txt.gz --create-dirs -o {PROJECT_HOME}/data/ca-HepPh/raw/ca-HepPh.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/ca-HepPh/raw/ca-HepPh.txt.gz")
     if dataset_name == "web-BerkStan":
         os.system(f"curl https://snap.stanford.edu/data/web-BerkStan.txt.gz --create-dirs -o {PROJECT_HOME}/data/web-BerkStan/raw/web-BerkStan.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/web-BerkStan/raw/web-BerkStan.txt.gz")
     if dataset_name == "web-Google":
         os.system(f"curl https://snap.stanford.edu/data/web-Google.txt.gz --create-dirs -o {PROJECT_HOME}/data/web-Google/raw/web-Google.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/web-Google/raw/web-Google.txt.gz")
     if dataset_name == "web-NotreDame":
         os.system(f"curl https://snap.stanford.edu/data/web-NotreDame.txt.gz --create-dirs -o {PROJECT_HOME}/data/web-NotreDame/raw/web-NotreDame.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/web-NotreDame/raw/web-NotreDame.txt.gz")
     if dataset_name == "web-Stanford":
         os.system(f"curl https://snap.stanford.edu/data/web-Stanford.txt.gz --create-dirs -o {PROJECT_HOME}/data/web-Stanford/raw/web-Stanford.txt.gz")
+        os.system(f"gzip -d {PROJECT_HOME}/data/web-Stanford/raw/web-Stanford.txt.gz")
     if dataset_name == "Reddit":
         dataset = PygDataset.Reddit(f"{PROJECT_HOME}/data/Reddit")
         # save test_data.data.edge_index to file
